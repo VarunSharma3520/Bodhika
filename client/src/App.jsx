d92f58d63218899124cd220ecc9c0adeffb50c69
VarunSharma3520 <sv279508@gmail.com>
2024-12-12 11:52:48 +0530

    intial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import constants from "./utils/constants";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p className="bg-slate-600 text-yellow-400 text-center py-32 text-5xl px-16">
+        {constants.home.images.nameimg}
+      </p>
+    </div>
+  );
+}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,19 @@
+const constants = {
+  home: {
+    images: { nameimg: "images/path", nameimg2: "images/path" },
+    video: { namevideo: "video/path00", namevideo2: "video/path00" },
+    text: { nametext: "lorem", nametext2: "lorem" },
+  },
+  admin: {
+    images: { nameimg: "images/path", nameimg2: "images/path" },
+    video: { namevideo: "video/path00", namevideo2: "video/path00" },
+    text: { nametext: "lorem", nametext2: "lorem" },
+  },
+  login: {
+    images: { nameimg: "images/path", nameimg2: "images/path" },
+    video: { namevideo: "video/path00", namevideo2: "video/path00" },
+    text: { nametext: "lorem", nametext2: "lorem" },
+  },
+};
+
+export default constants;
